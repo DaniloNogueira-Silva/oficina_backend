999824f66d512ddc0667e69edb5e33c9d068435b
DaniloNogueira-Silva <dansilva730x@gmail.com>
2024-03-08 21:35:37 -0300

    build: vercel3

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 /* eslint-disable prettier/prettier */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { PrismaService } from './prisma/prisma.service';
 
 async function bootstrap() {
+  const prisma = new PrismaService();
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }

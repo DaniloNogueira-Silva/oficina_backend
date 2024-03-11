fabee9f85911b8c943d61d3b4ef27887ab639964
DaniloNogueira-Silva <dansilva730x@gmail.com>
2024-03-11 10:19:16 -0300

    fix: enable cors

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,21 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { PrismaService } from './prisma/prisma.service';
 
-async function bootstrap() {
+async function bootstrap ()
+{
   const prisma = new PrismaService();
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create( AppModule );
+
+  // Configuração do CORS
+  app.enableCors( {
+    origin: '*', // Permitir requisições de qualquer origem, você pode ajustar conforme necessário
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Métodos HTTP permitidos
+    allowedHeaders: 'Content-Type,Authorization', // Headers permitidos
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  } );
+
+  await app.listen( 3000 );
 }
 bootstrap();
+
